68f060172d6f94346a2be51876b83b61b686da58
afnanMalek <142772064+afnanMalek@users.noreply.github.com>
2025-03-03 18:38:31 +0530

    Changes

--- src/Zod/validationSchema.tsx
+++ src/Zod/validationSchema.tsx
@@ -2,7 +2,9 @@ import { z } from "zod";
 
 export const formSchema = z.object({
   title: z.string().min(3, "Title must be at least 3 characters"),
-  description: z.string().min(5, "Description must be at least 5 characters"),
+  // description: z.string().min(5, "Description must be at least 5 characters"),
+  description: z.string().optional(),
+
   status: z.enum(["pending", "in-progress", "completed"]).optional(), // Making status optional
   statusId: z.number().int().min(1).max(3, "Invalid status ID"), 
 });
